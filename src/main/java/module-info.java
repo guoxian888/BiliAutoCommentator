df2a95cb5efe539514eaef00dab0b5760037d2cf
guoxian888 <2565088776@qq.com>
2024-03-14 15:19:27 +0800

    项目初始架构

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.axian.biliautocommentator {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.axian.biliautocommentator to javafx.fxml;
+    exports com.axian.biliautocommentator;
+}
